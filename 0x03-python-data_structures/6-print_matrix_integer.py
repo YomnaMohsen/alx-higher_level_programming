bfde03b07e65c0abfa11045e320e776ed572ad72
YomnaMohsen <ymn_mohsen@yahoo.com>
2024-01-07 23:02:09 +0200

    task 6

--- 0x03-python-data_structures/6-print_matrix_integer.py
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -2,5 +2,5 @@
 def print_matrix_integer(matrix=[[]]):
     for lists in matrix:
         for m in lists:
-            print("{:d}".format(m), end=" " if j != len(lists) - 1 else "")
+            print("{:d}".format(m), end=" " if m != lists[-1] - 1 else "")
         print("")

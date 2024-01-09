a4b295245e0a5c28acb045fd8c71269ae7d75954
YomnaMohsen <ymn_mohsen@yahoo.com>
2024-01-09 17:50:00 +0200

    fix task 13

--- 0x03-python-data_structures/lists.h
+++ 0x03-python-data_structures/lists.h
@@ -1,3 +1,4 @@
+
 #ifndef LISTS_H
 #define LISTS_H
 
@@ -10,7 +11,17 @@
  * Description: singly linked list node structure
  * for project
  */
-size_t list_len(const listint_t *h);
+typedef struct listint_s
+{
+	int n;
+	struct listint_s *next;
+} listint_t;
+
+size_t print_listint(const listint_t *h);
+listint_t *add_nodeint_end(listint_t **head, const int n);
+void free_listint(listint_t *head);
+
 int is_palindrome(listint_t **head);
+size_t list_len(const listint_t *h);
 
 #endif /* LISTS_H */

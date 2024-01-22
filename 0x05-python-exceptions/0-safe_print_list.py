cc54af698d3f28676d2b467d7be12c1457d60a0b
YomnaMohsen <ymn_mohsen@yahoo.com>
2024-01-22 22:01:58 +0200

    task 1

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -4,8 +4,8 @@ def safe_print_list(my_list=[], x=0):
     for j in range(x):
         try:
             print(my_list[j], end="")
-            i += 1
+            i += 1              
         except IndexError:
             break
-    print()
+    print()     
     return i

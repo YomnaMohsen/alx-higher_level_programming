c436063364c5aa31a4a978ba3fa811f56d6981eb
YomnaMohsen <ymn_mohsen@yahoo.com>
2024-01-22 22:19:16 +0200

    fix betty

--- 0x05-python-exceptions/2-safe_print_list_integers.py
+++ 0x05-python-exceptions/2-safe_print_list_integers.py
@@ -3,9 +3,9 @@ def safe_print_list_integers(my_list=[], x=0):
     c = 0
     for i in range(x):
         try:
-            if(isinstance(my_list[i],int)):
+            if (isinstance(my_list[i], int)):
                 print("{:d}".format(my_list[i]), end="")
-                c += 1            
+                c += 1
         except IndexError:
             raise
     print()

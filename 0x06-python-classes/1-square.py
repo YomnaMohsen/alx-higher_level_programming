d2c507b127aeaa4c9be641eb30f243853a0015b8
YomnaMohsen <ymn_mohsen@yahoo.com>
2024-01-24 22:22:40 +0200

    task 1

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+"""Square class"""
+
+
+class Square:
+    """This class defines a square shape"""
+
+
+    def __init__(self, size):
+     """Initializes a square object.
+
+     Args:
+
+        size (int): set square size
+     
+     """ 
+     self.__size = size  
+        

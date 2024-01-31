2a5288c46abfec30e4cf0d8a173d4cb0b269e53a
YomnaMohsen <ymn_mohsen@yahoo.com>
2024-01-31 23:06:42 +0200

    task 0

---
+++ 0x07-python-test_driven_development/0-add_integer.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+import sys
+"""add integer module """
+
+
+def add_integer(a, b=98):
+    """Return the addition of 2 int (a + b)
+    Args:
+        a: int number
+        b: int number
+    """
+    if not isinstance(a, (int, float)):
+        raise TypeError("a must be an integer")
+
+    if not isinstance(b, (int, float)):
+        raise TypeError("b must be an integer")
+
+    max = (2 ** 31) - 1
+    min = -max - 1
+    if a > max or a < min or b > max or b < min:
+        raise OverflowError("integer overflow")
+    return int(a) + int(b)

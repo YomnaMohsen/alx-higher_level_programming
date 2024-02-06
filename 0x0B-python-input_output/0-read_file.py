58c26f533627bb0cfb23caafd63ab8e508edc54f
YomnaMohsen <ymn_mohsen@yahoo.com>
2024-02-06 20:15:54 +0200

    fix task 0

--- 0x0B-python-input_output/0-read_file.py
+++ 0x0B-python-input_output/0-read_file.py
@@ -1,5 +1,12 @@
 #!/usr/bin/python3
+"""Read file module"""
+
+
 def read_file(filename=""):
+    """read_file
+       Args:
+       filename: string
+    """
     with open(filename, encoding="utf-8") as f:
         for line in f:
             print(line, end="")

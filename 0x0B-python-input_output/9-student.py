25504e0037a0e2f40bf88d493a90c91390c5eb6b
YomnaMohsen <ymn_mohsen@yahoo.com>
2024-02-07 23:20:39 +0200

    task 8

---
+++ 0x0B-python-input_output/9-student.py
@@ -0,0 +1,25 @@
+#!/usr/bin/python3
+"""Rectangle class"""
+
+
+class Student:
+    """This class defines student data"""
+    
+
+    def __init__(self, first_name, last_name, age):
+        """Initializes a student object.
+        Args:
+            firs_name (string): set student 1st name
+            last_name (string): set student last name
+            age (int): set student age
+        """
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def to_json(self):
+        """
+        retrieves a dictionary representation of 
+        a Student instance
+        """
+        return self.__dict__

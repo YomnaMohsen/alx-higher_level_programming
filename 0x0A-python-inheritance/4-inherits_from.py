7bb8dc024dce1214b683bfacf4953c9b901a547b
YomnaMohsen <ymn_mohsen@yahoo.com>
2024-02-09 23:53:22 +0200

    task 4

---
+++ 0x0A-python-inheritance/4-inherits_from.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+"""Define inherits_from module"""
+
+
+def inherits_from(obj, a_class):
+    """returns True if the object is an instance of,  a class that inherited
+    (directly or indirectly) from the specified class
+    otherwise False
+    Args:
+        obj (any object type)
+        a_class certain class to compare with
+    """
+    return issubclass(type(obj), a_class)

f37bf79b1d5c5abc15243fd1873eda0f709323f4
YomnaMohsen <ymn_mohsen@yahoo.com>
2024-02-11 00:18:44 +0200

    task 10

---
+++ 0x0A-python-inheritance/10-square.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+"""Square class module"""
+
+Rectangle = __import__("9-rectangle").Rectangle
+
+
+class Square(Rectangle):
+    """The Square class that inherits Recatangle"""
+
+    def __init__(self, size):
+        """Initializes square class
+            Args:
+            size :int
+        """
+        super().__init__(size, size)
--- 0x0A-python-inheritance/9-rectangle.py
+++ 0x0A-python-inheritance/9-rectangle.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
-"""Base Geometry class"""
+"""Rectangle class module"""
 
 BaseGeometry = __import__("7-base_geometry").BaseGeometry
 

0e67b7d0ac7eaeedc2df65c5dffbf81d7d2d3a09
YomnaMohsen <ymn_mohsen@yahoo.com>
2024-02-11 23:28:16 +0200

    task 11

---
+++ 0x0A-python-inheritance/11-square.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+"""Square class module"""
+
+Rectangle = __import__("9-rectangle").Rectangle
+
+
+class Square(Rectangle):
+    """The Square class that inherits Recatangle"""
+
+    def __init__(self, size):
+        """Initializes square class
+            Args:
+            size :int
+        """
+        super().integer_validator("size", size)
+        super().__init__(size, size)
+        self.__size = size
+
+    def __str__(self):
+        return "[Square] {}/{}".format(self.__size, self.__size)

590c3c71ff3e35df93c258d1ded87050c40ce840
YomnaMohsen <ymn_mohsen@yahoo.com>
2024-04-13 15:54:45 +0200

    task 4 & 5

--- 0x0E-SQL_more_queries/4-never_empty.sql
+++ 0x0E-SQL_more_queries/4-never_empty.sql
@@ -1,5 +1,4 @@
--- a script that creates the table id_not_null on your MySQL server
-CREATE TABLE IF NOT EXISTS force_name (
-    id INT Default 1,
-    name VARCHAR(256)
-);
+-- a script that creates the table id_not_null on your MySQL servers
+CREATE TABLE IF NOT EXISTS id_not_null (
+    id INT  Default  1,
+    name VARCHAR(256) );

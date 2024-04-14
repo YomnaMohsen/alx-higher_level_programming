0fb90f3d52b069c1eb956e609027cc24425abefc
YomnaMohsen <ymn_mohsen@yahoo.com>
2024-04-14 17:52:34 +0200

    fix

--- 0x0F-python-object_relational_mapping/1-filter_states.py
+++ 0x0F-python-object_relational_mapping/1-filter_states.py
@@ -13,6 +13,7 @@ if __name__ == "__main__":
         passwd=argv[2],
         db=argv[3])
     cur = db.cursor()
+    """BINARY to force case senstive"""
     cur.execute("SELECT * FROM states WHERE name LIKE BINARY 'N%';")
     states = cur.fetchall()
 

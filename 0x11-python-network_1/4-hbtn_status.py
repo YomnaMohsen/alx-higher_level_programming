4ff78a1839712b6b8c7c75fd6fcf12849dd8469d
YomnaMohsen <ymn_mohsen@yahoo.com>
2024-04-26 23:24:04 +0300

    task 4

--- 0x11-python-network_1/4-hbtn_status.py
+++ 0x11-python-network_1/4-hbtn_status.py
@@ -5,3 +5,7 @@ using requests package"""
 if __name__ == "__main__":
     import requests
     url = "https://alx-intranet.hbtn.io/status "
+    resp = requests.get(url)
+    print("Body response:")
+    print("\t- type: {}".format(type(resp.text)))
+    print("\t- content: {}".format(resp.text))

b2d7b019960f9f3c3933b3794dca8d58c2134a72
YomnaMohsen <ymn_mohsen@yahoo.com>
2024-04-26 23:06:36 +0300

    fix

--- 0x11-python-network_1/3-error_code.py
+++ 0x11-python-network_1/3-error_code.py
@@ -9,4 +9,4 @@ if __name__ == "__main__":
         with req.urlopen(sys.argv[1]) as response:
             print(response.read().decode("utf-8"))
     except error.HTTPError as e:
-        print("Error code:{}".format(e.code))
+        print("Error code:", e.code)

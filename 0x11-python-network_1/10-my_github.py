3ca93169da713f2a45f4cba6471b9fe216fe2cd4
YomnaMohsen <ymn_mohsen@yahoo.com>
2024-04-27 17:05:28 +0300

    t 9

---
+++ 0x11-python-network_1/10-my_github.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+"""Python script that takes your GitHub credentials (username and password)
+and uses the GitHub API to display your id, must use Basic Authentication
+with a personal access token as password to access to
+your information (only read:user permission is needed)
+"""
+
+if __name__ == "__main__":
+    import requests
+    import sys
+    head = {"Accept": "application/vnd.github+json",
+            "Authorization": "Bearer {}".format(sys.argv[2]),
+            "X-GitHub-Api-Version": "2022-11-28"
+            }
+    url = "https://api.github.com/user"
+    res = requests.get(url, headers=head)
+    print(res.json().get("id"))

7864ffe781ed94e90b5e3db8d61cc1293c59c34d
YomnaMohsen <ymn_mohsen@yahoo.com>
2024-05-21 23:40:59 +0300

    t 1

---
+++ 0x14-javascript-web_scraping/1-writeme.js
@@ -0,0 +1,11 @@
+#!/usr/bin/node
+const fs = require('node:fs');
+const content = process.argv[3];
+
+fs.writeFile(process.argv[2], content, err => {
+  if (err) {
+    console.error(err);
+  } else {
+    // file written successfully
+  }
+});

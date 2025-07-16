ebdd9433afdccb1caf26e7e83e01a1ef8c89a194
YomnaMohsen <ymn_mohsen@yahoo.com>
2025-07-16 16:32:26 +0300

    t1

---
+++ 0x04-python-more_data_structures/1-search_replace.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+def search_replace(my_list, search, replace):
+    """"replaces all occurrences of an element 
+    by another in a new list."""
+    
+    new_list = []
+    i = 0
+    while (i < len(my_list)):
+        if my_list[i] == search :
+            new_list.append(replace)
+        else :
+            new_list.append(my_list[i])      
+        i += 1
+    return new_list    
+
+     
+     

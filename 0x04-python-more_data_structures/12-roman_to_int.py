2f0710b314bb3c94f50a3dec9008f7ad6b717ec1
YomnaMohsen <ymn_mohsen@yahoo.com>
2025-07-28 18:22:01 +0300

    t12

--- 0x04-python-more_data_structures/12-roman_to_int.py
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -3,26 +3,12 @@ def roman_to_int(roman_string):
     sum = 0
     dict = {"I": 1, "V": 5, "X": 10, "L": 50,
     "C": 100, "D": 500, "M": 1000}
-    sub = ["IV", "IX", "XL", "XC", "CD", "CM"]
-    sub_st = " "
     if not (isinstance(roman_string, str)) or roman_string is None:
         return 0
-    i = 0
-    while (i < len(roman_string)):
-        if (i == (len(roman_string) - 1)):
-            sum += dict[roman_string[i]]
-            break
-        sub_st = roman_string[i] + roman_string[i + 1]
-        if is_sub(sub, sub_st):
-            sum += dict[roman_string[i + 1]] - dict[roman_string[i]]
-            i += 2
+   
+    for i in range(len(roman_string)):
+        if i + 1 < len(roman_string) and dict[roman_string[i]] < dict[roman_string[i + 1]]:
+            sum -= dict[roman_string[i]]
         else:
             sum += dict[roman_string[i]]
-            i += 1
-        return sum
-
-
-def is_sub(s, s2):
-    if (s2 in s):
-        return True
-    return False
+    return sum           

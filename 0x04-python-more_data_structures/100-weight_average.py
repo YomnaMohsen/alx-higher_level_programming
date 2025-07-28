d8eeeb4618e9667e402406a682add756eee6314a
YomnaMohsen <ymn_mohsen@yahoo.com>
2025-07-28 19:11:39 +0300

    t14

---
+++ 0x04-python-more_data_structures/100-weight_average.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+def weight_average(my_list=[]):
+    if  len(my_list) == 0:
+        return 0
+    
+    sum = 0
+    avg = 0
+    
+    for tup in my_list:
+        sum += tup[0] * tup[1]
+        avg += tup[1]
+    return sum / avg
+
+ #n1 = sum(map(lambda i: i[0] * i[1], my_list))
+ #n2 = sum(map(lambda i: i[1], my_list))
+        
